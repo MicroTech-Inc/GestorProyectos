e6f7655e098c2b9d937c71446b63eebc9b61c70f
juanemontoya <jemontoya@gmail.com>
2021-11-29 08:57:07 -0500

    Front base

---
+++ src/pages/Perfil/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IndexPerfil = () => {
+  return <div>Este es el index de Perfil</div>;
+};
+
+export default IndexPerfil;
---
+++ src/pages/proyectos/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IndexProyectos = () => {
+  return <div>Este es el index de Proyectos</div>;
+};
+
+export default IndexProyectos;
