705ccfc91d07d6e21cfea325885116e1b0ad09e5
Juan Esteban Montoya <90530464+juanemontoya@users.noreply.github.com>
2021-11-29 08:46:19 -0500

    Actualizacion Front

---
+++ src/pages/avances/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IndexAvances = () => {
+  return <div>Este es el index de Avances</div>;
+};
+
+export default IndexAvances;
---
+++ src/pages/inscripciones/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IndexInscripciones = () => {
+  return <div>Este es el index de Inscripciones</div>;
+};
+
+export default IndexInscripciones;
